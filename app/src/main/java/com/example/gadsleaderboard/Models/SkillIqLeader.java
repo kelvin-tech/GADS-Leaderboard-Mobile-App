c4b8a49819d0fedda729c8ebf6c2f5e3a24d7bbe
kelvin-tech <41630916+kelvin-tech@users.noreply.github.com>
2020-09-10 14:38:28 +0000

    update

---
+++ app/src/main/java/com/example/gadsleaderboard/Models/SkillIqLeader.java
@@ -0,0 +1,35 @@
+package com.example.gadsleaderboard.Models;
+
+/**
+ * created by Kelvin.(github:kelvin-tech).
+ */
+public class SkillIqLeader {
+
+    private String name;
+    private int score;
+    private String country;
+    private String badgeUrl;
+
+    public SkillIqLeader(String name, int score, String country, String badgeUrl) {
+        this.name = name;
+        this.score = score;
+        this.country = country;
+        this.badgeUrl = badgeUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getBadgeUrl() {
+        return badgeUrl;
+    }
+}
